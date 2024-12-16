faa6f5816d0fd4dc6ac44feabd67282074b28631
Sahil Kale <sahil.kale1@gmail.com>
2024-12-16 00:45:06 -0500

    Fix temp paths

--- pdf_combiner.py
+++ pdf_combiner.py
@@ -11,18 +11,30 @@ class PdfCombiner:
         self.tmp_paths = []
 
         for i in range(len(self.input_paths)):
+            output_path_dir = os.path.dirname(self.output_path)
             if self.input_paths[i].endswith((".png", ".jpg", ".jpeg")):
                 tmp_path = self.input_paths[i]
-                # strip the extension
+                tmp_path = os.path.basename(tmp_path)
+                # get the filename without the extension
                 tmp_path = tmp_path[: tmp_path.rfind(".")]
+
+                # add the output path directory
+                tmp_path = os.path.join(output_path_dir, tmp_path)
+
+                # remove the extension and add _TMP.pdf
                 tmp_path += "_TMP.pdf"
                 image_to_pdf(self.input_paths[i], tmp_path)
                 self.input_paths[i] = tmp_path
+                self.tmp_paths.append(tmp_path)
 
         self.combine_pdfs()
 
-        for tmp_path in self.tmp_paths:
-            os.remove(tmp_path)
+    def __del__(self):
+        for path in self.tmp_paths:
+            try:
+                os.remove(path)
+            except FileNotFoundError:
+                pass
 
     def combine_pdfs(self):
         pdf = pikepdf.Pdf.new()
